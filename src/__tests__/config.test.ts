b5feb52a5f58810c8d55c97db5265db1cbff1038
AF <alessandro.ferlin@pagopa.it>
2021-12-14 10:59:11 +0100

    Lint code

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -1,7 +1,7 @@
 import * as C from "../config";
 import * as E from "fp-ts/Either";
-import * as DE from 'io-ts/DecodeError'
-import * as FS from 'io-ts/FreeSemigroup'
+import * as DE from "io-ts/DecodeError";
+import * as FS from "io-ts/FreeSemigroup";
 
 describe("Config", () => {
   // TODO integrate with property base test
@@ -12,18 +12,35 @@ describe("Config", () => {
         SERVER_PORT: "n",
       };
 
-      const validLogLevel =
-        `"debug" | "trace" | "error" | "info" | "fatal" | "warn"`
+      const validLogLevel = `"debug" | "trace" | "error" | "info" | "fatal" | "warn"`;
 
       const expected = E.left(
         FS.concat(
           FS.concat(
-            FS.of(DE.key("LOG_LEVEL", DE.required, FS.of(DE.leaf(undefined, validLogLevel)))),
-            FS.of(DE.key("SERVER_HOSTNAME", DE.required, FS.of(DE.leaf(undefined, "string")))),
+            FS.of(
+              DE.key(
+                "LOG_LEVEL",
+                DE.required,
+                FS.of(DE.leaf(undefined, validLogLevel))
+              )
+            ),
+            FS.of(
+              DE.key(
+                "SERVER_HOSTNAME",
+                DE.required,
+                FS.of(DE.leaf(undefined, "string"))
+              )
+            )
           ),
-          FS.of(DE.key("SERVER_PORT", DE.required, FS.of(DE.leaf(NaN, "Not a number")))),
+          FS.of(
+            DE.key(
+              "SERVER_PORT",
+              DE.required,
+              FS.of(DE.leaf(NaN, "Not a number"))
+            )
+          )
         )
-      )
+      );
       const actual = C.parseConfig(confEnv);
       expect(actual).toStrictEqual(expected);
     });
@@ -35,7 +52,7 @@ describe("Config", () => {
         SERVER_HOSTNAME: "hostname",
         SERVER_PORT: "1234",
         LOG_LEVEL: "trace",
-        APPLICATION_NAME: "application"
+        APPLICATION_NAME: "application",
       };
 
       const expected = E.right({
