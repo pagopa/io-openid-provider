59775e50c581b98f5bb1ff63b5a7620b42233583
AF <alessandro.ferlin@pagopa.it>
2021-12-14 18:22:39 +0100

    Add test on router

--- src/info/__tests__/router.test.ts
+++ src/info/__tests__/router.test.ts
@@ -1,3 +1,5 @@
+import express from "express";
+import request from "supertest";
 import * as router from "../router";
 import * as records from "../../__tests__/utils/records";
 import * as responses from "@pagopa/ts-commons/lib/responses";
@@ -10,5 +12,14 @@ describe("getInfoEndpointHandler", () => {
     // TODO: Find a better way to define this assert, e.g. with one expect
     expect(actual.kind).toStrictEqual(expected.kind);
     expect(actual).toMatchObject({ value: expected.value });
+  });
+});
+
+describe("makeRouter", () => {
+  it("should return a workable router", async () => {
+    const application = express().use(router.makeRouter(records.validConfig));
+    const actual = await request(application).get("/info/api");
+
+    expect(actual.statusCode).toBe(200);
   });
 });
