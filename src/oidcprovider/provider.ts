f9ad1f9539ff6afb7caf24b1c3ede5419b22bb7f
AF <alessandro.ferlin@pagopa.it>
2022-01-05 10:24:16 +0100

    Move the provider on its own file

---
+++ src/oidcprovider/provider.ts
@@ -0,0 +1,77 @@
+import * as b from "fp-ts/boolean";
+import * as TE from "fp-ts/TaskEither";
+import * as f from "fp-ts/lib/function";
+import * as u from "src/userinfo";
+import * as c from "src/config";
+import * as oidc from "oidc-provider";
+import * as redis from "./dal/redis";
+
+const userInfoToAccount = (userInfo: u.UserInfo): oidc.Account => ({
+  accountId: userInfo.id,
+  claims: (_use: string, _scope: string) => ({
+    sub: userInfo.id,
+  }),
+});
+
+const findAccountAdapter =
+  (userInfoClient: u.UserInfoClient): oidc.FindAccount =>
+  (_ctx, id) =>
+    f.pipe(
+      userInfoClient.findUserByFederationToken(id),
+      TE.map(userInfoToAccount),
+      TE.mapLeft((_) => undefined),
+      TE.toUnion
+    )();
+
+const makeProvider = (
+  config: c.Config,
+  userInfoClient: u.UserInfoClient,
+  dbInMemory: boolean
+): oidc.Provider => {
+  // use a named function because of https://github.com/panva/node-oidc-provider/issues/799
+  function adapter(str: string) {
+    return redis.makeRedisAdapter(config.redis)(str);
+  }
+  const adapterConfig = b.fold(
+    () => ({ adapter }),
+    () => ({})
+  )(dbInMemory);
+
+  const providerConfig: oidc.Configuration = {
+    ...adapterConfig,
+    clients: [
+      {
+        client_id: "foo",
+        client_secret: "bar",
+        grant_types: ["implicit"],
+        redirect_uris: ["https://client.example.org/cb"],
+        response_types: ["id_token"],
+        token_endpoint_auth_method: "none",
+      },
+    ],
+    features: {
+      devInteractions: {
+        enabled: true,
+      },
+      rpInitiatedLogout: {
+        enabled: false,
+      },
+      userinfo: {
+        enabled: false,
+      },
+    },
+    findAccount: findAccountAdapter(userInfoClient),
+    responseTypes: ["id_token"],
+    routes: {
+      authorization: "/oauth/authorize",
+    },
+    scopes: ["openid"],
+    tokenEndpointAuthMethods: ["none"],
+  };
+  return new oidc.Provider(
+    `https://${config.server.hostname}:${config.server.port}`,
+    providerConfig
+  );
+};
+
+export { makeProvider };
--- src/oidcprovider/router.ts
+++ src/oidcprovider/router.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import * as oidc from "oidc-provider";
-import * as b from "fp-ts/boolean";
 import * as O from "fp-ts/Option";
 import * as T from "fp-ts/Task";
 import * as TE from "fp-ts/TaskEither";
@@ -8,7 +7,7 @@ import { flow, pipe } from "fp-ts/lib/function";
 import { Config } from "src/config";
 import * as u from "src/userinfo";
 import * as strings from "@pagopa/ts-commons/lib/strings";
-import * as redis from "./dal/redis";
+import * as p from "./provider";
 
 // TODO: Move to environment
 const cookieKey = "X-IO-Federation-Token";
@@ -27,7 +26,7 @@ const userInfoClientErrorToInteractionResults = (
   error: u.UserInfoClientError
 ): oidc.InteractionResults => ({
   error: error.errorType,
-  error_description: "Not authorized",
+  error_description: "Unauthorized",
 });
 
 const interactionLogic = (
@@ -54,7 +53,7 @@ const interactionLogic = (
           ),
           TE.map(userInfoToInteractionResults),
           TE.toUnion,
-          T.map((_) => O.some(_))
+          T.map(flow(O.some))
         )
     )
   );
@@ -80,80 +79,12 @@ const interactionHandler =
       )
     )();
 
-const userInfoToAccount = (userInfo: u.UserInfo): oidc.Account => ({
-  accountId: userInfo.id,
-  claims: (_use: string, _scope: string) => ({
-    sub: userInfo.id,
-  }),
-});
-
-const findAccountAdapter =
-  (userInfoClient: u.UserInfoClient): oidc.FindAccount =>
-  (_ctx, id) =>
-    pipe(
-      userInfoClient.findUserByFederationToken(id),
-      TE.map(userInfoToAccount),
-      TE.mapLeft((_) => undefined),
-      TE.toUnion
-    )();
-
-const makeProvider = (
-  config: Config,
-  userInfoClient: u.UserInfoClient,
-  dbInMemory: boolean
-): oidc.Provider => {
-  // use a named function because of https://github.com/panva/node-oidc-provider/issues/799
-  function adapter(str: string) {
-    return redis.makeRedisAdapter(config.redis)(str);
-  }
-  const adapterConfig = b.fold(
-    () => ({ adapter }),
-    () => ({})
-  )(dbInMemory);
-
-  const providerConfig: oidc.Configuration = {
-    ...adapterConfig,
-    clients: [
-      {
-        client_id: "foo",
-        client_secret: "bar",
-        grant_types: ["implicit"],
-        redirect_uris: ["https://client.example.org/cb"],
-        response_types: ["id_token"],
-        token_endpoint_auth_method: "none",
-      },
-    ],
-    features: {
-      devInteractions: {
-        enabled: true,
-      },
-      rpInitiatedLogout: {
-        enabled: false,
-      },
-      userinfo: {
-        enabled: false,
-      },
-    },
-    findAccount: findAccountAdapter(userInfoClient),
-    responseTypes: ["id_token"],
-    routes: {
-      authorization: "/oauth/authorize",
-    },
-    scopes: ["openid"],
-    tokenEndpointAuthMethods: ["none"],
-  };
-  return new oidc.Provider(
-    `https://${config.server.hostname}:${config.server.port}`,
-    providerConfig
-  );
-};
-
 const makeRouter = (
   config: Config,
   userInfoClient: u.UserInfoClient,
   dbInMemory: boolean
 ): express.Router => {
-  const provider = makeProvider(config, userInfoClient, dbInMemory);
+  const provider = p.makeProvider(config, userInfoClient, dbInMemory);
 
   const router = express.Router();
 
