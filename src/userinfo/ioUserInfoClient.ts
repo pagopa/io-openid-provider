caa7cd8d00ff94a7ace6e743e140575992aaf9d0
Marco Comi <marco.comi@pagopa.it>
2022-01-27 14:38:08 +0100

    Refactor on method that get user info from IO-Backend

--- src/userinfo/ioUserInfoClient.ts
+++ src/userinfo/ioUserInfoClient.ts
@@ -1,32 +1,55 @@
 import * as TE from "fp-ts/lib/TaskEither";
 import * as f from "fp-ts/function";
 import * as authClient from "../generated/clients/io-auth/client";
+import { UserIdentity } from "../generated/clients/io-auth/UserIdentity";
 import * as I from ".";
 
-const error: I.UserInfoClientError = {
-  errorType: "unknown",
-};
+const toError = (type: I.ErrorType): I.UserInfoClientError => ({
+  errorType: type,
+});
 
-// TODO: This method is not yet complete
+const makeUserInfo = (identity: UserIdentity): I.UserInfo => ({
+  id: identity.fiscal_code,
+});
+
+const getUserInfoFromIOBackend =
+  (client: authClient.Client) => (federationToken: I.FederationToken) =>
+    f.pipe(
+      TE.tryCatch(
+        () => client.getUserIdentity({ Bearer: federationToken }),
+        () => toError("unknown")
+      )
+    );
+
 const findUserBySession =
   (client: authClient.Client) =>
-  (id: I.FederationToken): TE.TaskEither<I.UserInfoClientError, I.UserInfo> =>
+  (
+    federationToken: I.FederationToken
+  ): TE.TaskEither<I.UserInfoClientError, I.UserInfo> =>
     f.pipe(
-      f.constant(client.getUserIdentity({ Bearer: id })),
-      TE.fromTask,
-      TE.chain(TE.fromEither),
-      TE.mapLeft((_) => error),
-      TE.chain((_) => {
-        switch (_.status) {
+      federationToken,
+      getUserInfoFromIOBackend(client),
+      TE.chainW(
+        f.flow(
+          TE.fromEither,
+          TE.mapLeft((_) => toError("decoding"))
+        )
+      ),
+      TE.chain((response) => {
+        switch (response.status) {
           case 200:
-            return TE.right(_.value);
+            return TE.right(response.value);
           case 400:
-            return TE.left(error);
+            return TE.left(toError("badRequest"));
+          case 401:
+            return TE.left(toError("invalidToken"));
+          case 500:
+            return TE.left(toError("unknown"));
           default:
-            return TE.left(error);
+            return TE.left(toError("unknown"));
         }
       }),
-      TE.map((_) => ({ id: _.fiscal_code }))
+      TE.map(makeUserInfo)
     );
 
 const makeIOUserInfoClient = (client: authClient.Client): I.UserInfoClient => ({
