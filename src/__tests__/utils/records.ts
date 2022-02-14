976db058e27616898cda1cfbf83455059b3d0331
AF <alessandro.ferlin@pagopa.it>
2022-02-14 14:47:23 +0100

    Revert "Change type of redis url in records file"
    This reverts commit 6201af3b80116384969029e4aace0186899c8990.

--- src/__tests__/utils/records.ts
+++ src/__tests__/utils/records.ts
@@ -29,7 +29,7 @@ const validConfig: config.Config = {
     logName: "application",
   },
   redis: {
-    url: "http://localhost:3791",
+    url: new URL("http://localhost:3791"),
     keyPrefix: "oidc:",
   },
 };
