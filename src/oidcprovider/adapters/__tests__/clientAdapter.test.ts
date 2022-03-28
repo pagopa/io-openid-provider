2c9d3316e158151746880a6c102b235ac17b65bd
AF <alessandro.ferlin@pagopa.it>
2022-03-28 18:00:55 +0200

    Improve clientAdapter tests

--- src/oidcprovider/adapters/__tests__/clientAdapter.test.ts
+++ src/oidcprovider/adapters/__tests__/clientAdapter.test.ts
@@ -7,26 +7,19 @@ import { notImplementedError } from "../utils";
 import { constVoid } from "fp-ts/lib/function";
 import { client } from "../../../core/__tests__/data";
 
-describe("makeClientAdapter", () => {
-  it("should override the correct functions", async () => {
-    const id = client.client_id;
-    const loggerMock = mock.mock<Logger>();
-    const clientRepositoryMock = mock.mock<ClientRepository>();
-    const adapter = makeClientAdapter(loggerMock, clientRepositoryMock);
+const makeAdapterForTest = () => {
+  const loggerMock = mock.mock<Logger>();
+  const clientRepositoryMock = mock.mock<ClientRepository>();
+  const adapter = makeClientAdapter(loggerMock, clientRepositoryMock);
+  return { loggerMock, clientRepositoryMock, adapter };
+};
 
-    clientRepositoryMock.remove
-      .calledWith(id)
-      .mockReturnValueOnce(TE.right(constVoid()));
-    clientRepositoryMock.find
-      .calledWith(id)
-      .mockReturnValueOnce(TE.right(client));
-    clientRepositoryMock.upsert
-      .calledWith(client)
-      .mockReturnValueOnce(TE.right(client));
+describe("makeClientAdapter", () => {
+  it("should return 'not implemented' error useless functions", async () => {
+    const id = client.client_id;
+    const { adapter } = makeAdapterForTest();
 
     await expect(adapter.consume(id)).rejects.toThrowError(notImplementedError);
-    await expect(adapter.destroy(id)).resolves.toStrictEqual(constVoid());
-    await expect(adapter.find(id)).resolves.toStrictEqual(client);
     await expect(adapter.findByUid(id)).rejects.toThrowError(
       notImplementedError
     );
@@ -36,17 +29,51 @@ describe("makeClientAdapter", () => {
     await expect(adapter.revokeByGrantId(id)).rejects.toThrowError(
       notImplementedError
     );
-    await expect(adapter.upsert(id, client, 123)).resolves.toStrictEqual(
-      constVoid()
-    );
   });
   describe("upsert", () => {
     it("should fail given a bad input", async () => {
-      const loggerMock = mock.mock<Logger>();
-      const clientRepositoryMock = mock.mock<ClientRepository>();
-      const adapter = makeClientAdapter(loggerMock, clientRepositoryMock);
+      const { adapter } = makeAdapterForTest();
 
       await expect(adapter.upsert("id", {}, 123)).rejects.toThrow();
+    });
+    it("should call the upsert function of the given repository", async () => {
+      const id = client.client_id;
+      const { clientRepositoryMock, adapter } = makeAdapterForTest();
+
+      const recorder = clientRepositoryMock.upsert
+        .calledWith(client)
+        .mockReturnValueOnce(TE.right(client));
+
+      await expect(adapter.upsert(id, client, 123)).resolves.toStrictEqual(
+        constVoid()
+      );
+      expect(recorder).toBeCalledTimes(1);
+    });
+  });
+  describe("find", () => {
+    it("should call the find function of the given repository", async () => {
+      const id = client.client_id;
+      const { clientRepositoryMock, adapter } = makeAdapterForTest();
+
+      const recorder = clientRepositoryMock.find
+        .calledWith(id)
+        .mockReturnValueOnce(TE.right(client));
+
+      await expect(adapter.find(id)).resolves.toStrictEqual(client);
+      expect(recorder).toBeCalledTimes(1);
+    });
+  });
+  describe("destroy", () => {
+    it("should call the remove function of the given repository", async () => {
+      const id = client.client_id;
+      const { clientRepositoryMock, adapter } = makeAdapterForTest();
+
+      const recorder = clientRepositoryMock.remove
+        .calledWith(id)
+        .mockReturnValueOnce(TE.right(constVoid()));
+
+      await expect(adapter.destroy(id)).resolves.toStrictEqual(constVoid());
+      expect(recorder).toBeCalledTimes(1);
     });
   });
 });
