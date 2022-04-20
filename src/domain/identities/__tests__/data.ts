47610cb863250cb122bb0c0e03d3e864dd47710e
AF <alessandro.ferlin@pagopa.it>
2022-04-20 20:49:56 +0200

    Add test on IdentityService implemented using IO

--- src/domain/identities/__tests__/data.ts
+++ src/domain/identities/__tests__/data.ts
@@ -2,8 +2,8 @@ import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { Identity, IdentityId } from "../types";
 
 export const identity: Identity = {
-  familyName: "FamilyName",
-  fiscalCode: "FISCALCODE" as NonEmptyString,
-  givenName: "GivenName",
+  familyName: "Asdrubale",
+  fiscalCode: "TMMEXQ60A10Y526X" as NonEmptyString,
+  givenName: "Roitek",
   id: "identity-id" as IdentityId,
 };
