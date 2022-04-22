a5ca8c49013e9fdc22eb0a90c1f85b9457269c41
AF <alessandro.ferlin@pagopa.it>
2022-04-22 08:51:50 +0200

    Apply code-review suggestion

--- src/domain/utils.ts
+++ src/domain/utils.ts
@@ -1,6 +1,5 @@
 import { pipe } from "fp-ts/lib/function";
 import { Option } from "fp-ts/lib/Option";
-import { TaskEither } from "fp-ts/lib/TaskEither";
 import * as TE from "fp-ts/lib/TaskEither";
 import { DomainError, DomainErrorTypes, makeDomainError } from "./types";
 
@@ -10,8 +9,8 @@ export const show = <T>(_: T) => JSON.stringify(_, null, 2);
  * Given a Task<Either<L, Option<R>>> return a Task<Either<L, R>>
  */
 export const fromTEOtoTE = <T>(
-  teo: TaskEither<DomainError, Option<T>>
-): TaskEither<DomainError, T> =>
+  teo: TE.TaskEither<DomainError, Option<T>>
+): TE.TaskEither<DomainError, T> =>
   pipe(
     teo,
     TE.chain(
