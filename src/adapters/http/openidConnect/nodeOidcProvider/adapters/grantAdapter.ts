9c8be1740ea98db20dd7ddf585d3f2e5e56e0775
AF <alessandro.ferlin@pagopa.it>
2022-05-11 17:02:36 +0200

    Remove useless code from node-oidc grantAdapter

--- src/adapters/http/openidConnect/nodeOidcProvider/adapters/grantAdapter.ts
+++ src/adapters/http/openidConnect/nodeOidcProvider/adapters/grantAdapter.ts
@@ -1,16 +1,11 @@
 import * as oidc from "oidc-provider";
-import * as t from "io-ts";
-import { pipe } from "fp-ts/function";
-import * as E from "fp-ts/Either";
 import { Logger } from "../../../../../domain/logger";
 import { GrantService } from "../../../../../domain/grants/GrantService";
 import { Grant } from "../../../../../domain/grants/types";
-import { Client, ClientId } from "../../../../../domain/clients/types";
 import {
   makeNotImplementedAdapter,
   findFromTEO,
   destroyFromTE,
-  upsertFromTE,
   IdentityIdAndGrantId,
   DateFromNumericDate,
 } from "../utils";
@@ -30,34 +25,6 @@ export const grantToAdapterPayload = (entity: Grant): oidc.AdapterPayload => ({
   },
 });
 
-const adapterPayloadToGrant = (
-  payload: oidc.AdapterPayload
-): t.Validation<Grant> =>
-  pipe(
-    E.of(
-      ([idnId, grnId]: IdentityIdAndGrantId) =>
-        (clientId: ClientId) =>
-        (exp: Date) =>
-        (iat: Date) =>
-        (scope: string) => ({
-          expireAt: exp,
-          id: grnId,
-          issuedAt: iat,
-          remember: undefined,
-          scope,
-          subjects: {
-            clientId,
-            identityId: idnId,
-          },
-        })
-    ),
-    E.ap(IdentityIdAndGrantId.decode(payload.jti)),
-    E.ap(Client.props.clientId.decode(payload.clientId)),
-    E.ap(DateFromNumericDate.decode(payload.exp)),
-    E.ap(DateFromNumericDate.decode(payload.iat)),
-    E.ap(Grant.props.scope.decode(payload.openid?.scope))
-  );
-
 export const makeGrantAdapter = (
   logger: Logger,
   grantService: GrantService
@@ -75,11 +42,5 @@ export const makeGrantAdapter = (
     IdentityIdAndGrantId.decode,
     grantToAdapterPayload,
     ([identityId, grantId]) => grantService.find(identityId, grantId)
-  ),
-  upsert: upsertFromTE(
-    logger,
-    "Grant",
-    adapterPayloadToGrant,
-    grantService.upsert
   ),
 });
