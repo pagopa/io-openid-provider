a15181296357646b7d7de2ff801cc69e987c9d62
AF <alessandro.ferlin@pagopa.it>
2022-05-16 14:19:23 +0200

    Generate JWK just for testing purposes

--- src/__tests__/data.ts
+++ src/__tests__/data.ts
@@ -1,8 +1,29 @@
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { Config } from "../config";
 import { Seconds } from "../domain/types";
+import { generateKeyPairSync } from "crypto";
+import { pem2jwk } from "pem-jwk";
 
-export const envs = process.env;
+// Generate a private key just for testing purposes
+const { privateKey } = generateKeyPairSync("rsa", {
+  modulusLength: 4096,
+  publicKeyEncoding: {
+    type: "spki",
+    format: "pem",
+  },
+  privateKeyEncoding: {
+    type: "pkcs1",
+    format: "pem",
+  },
+});
+
+const privateJwk = pem2jwk(privateKey);
+
+export const envs: NodeJS.ProcessEnv = {
+  ...process.env,
+  JWK_PRIMARY: JSON.stringify(privateJwk),
+  JWK_SECONDARY: JSON.stringify(privateJwk),
+};
 
 export const config: Config = {
   IOClient: {
@@ -27,5 +48,8 @@ export const config: Config = {
     enableProxy: false,
     hostname: envs["SERVER_HOSTNAME"] as string,
     port: envs["PORT"] as string,
+    cookiesKey: envs["COOKIES_KEY"] as string,
+    jwkPrimary: envs["JWK_PRIMARY"] as string,
+    jwkSecondary: envs["JWK_SECONDARY"] as string,
   },
 };
