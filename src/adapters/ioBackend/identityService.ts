6bd44c3dce7931a2e1d53d550eb0480317d22274
AF <4775679+datalek@users.noreply.github.com>
2023-07-12 17:37:18 +0200

    Revert "Add log to identityService (#88)"
    This reverts commit bfca6aa6065788aeb6db0aa749ad202c332336c1.

--- src/adapters/ioBackend/identityService.ts
+++ src/adapters/ioBackend/identityService.ts
@@ -14,15 +14,8 @@ const authenticate =
     pipe(
       TE.tryCatch(
         () => client.getUserIdentity({ Bearer: `Bearer ${token}` }),
-        (_) => {
-          logger.debug(`authenticate getUserIdentity: ${_}`);
-          return makeDomainError("Unexpected", DomainErrorTypes.GENERIC_ERROR);
-        }
+        (_) => makeDomainError("Unexpected", DomainErrorTypes.GENERIC_ERROR)
       ),
-      TE.map((response) => {
-        logger.debug(`authenticate getUserIdentity: ${show(response)}`);
-        return response;
-      }),
       TE.chainEitherK(
         flow(
           E.mapLeft((_) =>
