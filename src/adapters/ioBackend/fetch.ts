7bfe6f6aeba11b132e56168ed11b2cc245260ea2
AF <4775679+datalek@users.noreply.github.com>
2023-07-12 17:23:28 +0200

    Change agent (#89)
    * Add log to identityService
    
    * Change the agent of io client

--- src/adapters/ioBackend/fetch.ts
+++ src/adapters/ioBackend/fetch.ts
@@ -5,12 +5,11 @@ import { Millisecond } from "@pagopa/ts-commons/lib/units";
 // HTTP external requests timeout in milliseconds
 const DEFAULT_REQUEST_TIMEOUT_MS = 10000;
 
-// HTTP-only fetch with optional keepalive agent
 // @see https://github.com/pagopa/io-ts-commons/blob/master/src/agent.ts#L10
-const httpApiFetch = agent.getHttpFetch(process.env);
+const apiFetch = agent.getFetch(process.env);
 
 // a fetch that can be aborted and that gets cancelled after fetchTimeoutMs
-const abortableFetch = fetch.AbortableFetch(httpApiFetch);
+const abortableFetch = fetch.AbortableFetch(apiFetch);
 
 const timeoutFetch = fetch.toFetch(
   fetch.setFetchTimeout(
